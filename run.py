8df4cf55ac6dc21ccaa8ea66721794dd7cf029c7
Krishna Sai V <krishnakittu2421@gmail.com>
2025-08-02 23:18:10 +0530

    updated app running logic

--- run.py
+++ run.py
@@ -1,4 +1,24 @@
+import datetime
+import time
 from bot import start_bot
 
+def is_active_hours():
+    now = datetime.datetime.now().time()
+    start_time = datetime.time(10,0)
+    end_time = datetime.time(0,0)
+    
+    return start_time <= now or now < end_time
+
 if __name__ == "__main__":
-    start_bot()
+    while True:
+        if is_active_hours():
+            print("Active hours - running bot.")
+            try:
+             start_bot()
+            except Exception as e:
+                print("Error: ",e)
+                time.sleep(60)
+                
+        else:
+            print("Outside active hours, Sleeping....")
+            time.sleep(600)
